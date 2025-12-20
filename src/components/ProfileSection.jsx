fbca9b2f93aa815366e965cc7b67e6f0da4a991d
David Thomason <911671+dtom90@users.noreply.github.com>
2025-12-20 14:14:18 -0500

    fixed padding issue

--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -1,6 +1,6 @@
 function ProfileSection() {
   return (
-    <div className="flex flex-col py-24 w-[522px]">
+    <div className="flex flex-col px-8">
       <h1 className="text-text-primary font-semibold m-0 text-heading-1">
         <a href="/" className="hover:text-text-secondary transition-colors no-underline">David Thomason</a>
       </h1>
