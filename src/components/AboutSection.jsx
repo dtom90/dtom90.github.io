2d67a7e254dd78a9d07e6af6b22a093353a4a22e
David Thomason <911671+dtom90@users.noreply.github.com>
2026-01-17 13:22:59 -0500

    titles

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,10 +3,14 @@ import aboutContent from '../content/about.md?raw'
 
 function AboutSection() {
   return (
-    <MarkdownSection
-      id="about"
-      content={aboutContent}
-    />
+    <div className="max-w-xl text-left" id="about">
+      <div className="h-20 flex-shrink-0"></div>
+      <div className="text-heading-2 font-semibold mb-8 text-text-primary">About</div>
+      <MarkdownSection
+        content={aboutContent}
+      />
+      <div className="h-20 flex-shrink-0"></div>
+    </div>
   )
 }
 
