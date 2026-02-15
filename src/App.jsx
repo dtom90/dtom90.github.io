a2571e53249704a42acfee77575d96ff2667d54d
David Thomason <911671+dtom90@users.noreply.github.com>
2026-02-15 13:41:40 -0500

    fix profile

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center px-8 bg-background-primary">
       <div className="flex flex-col md:flex-row items-start justify-between gap-12 max-w-6xl w-full h-screen max-h-screen py-8 overflow-y-auto md:overflow-hidden">
-        <div className="flex-1 max-w-[50%] md:h-full py-20 md:overflow-y-auto">
+        <div className="flex-1 max-w-[50%] md:h-full md:overflow-y-auto flex flex-col justify-center">
           <ProfileSection />
         </div>
         <div ref={scrollContainerRef} className="flex flex-col md:overflow-y-auto flex-1 min-w-0 md:h-full">
